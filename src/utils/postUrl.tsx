689b9bf317970331536c76a0491c35407bd1c403
Jim Martens <github@2martens.de>
2026-01-11 16:26:46 +0100

    fix: Remove reference to speeches

--- src/utils/postUrl.tsx
+++ src/utils/postUrl.tsx
@@ -1,5 +1,5 @@
 import type { CollectionEntry } from "astro:content";
 
-export function buildPostUrl(post: CollectionEntry<"posts">|CollectionEntry<"speeches">) {
+export function buildPostUrl(post: CollectionEntry<"posts">) {
     return `${post.data.publishedAt.toISOString().split('T')[0] + '-' + post.data.slug + '.html'}`;
 }
